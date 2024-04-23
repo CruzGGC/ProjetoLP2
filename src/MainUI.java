1602a37faf5d24d4ff66e4a21420a121e932604d
Tiago-sc <tiagosc@ua.pt>
2024-04-23 16:58:13 +0100

    Add UImain

--- src/MainUI.java
+++ src/MainUI.java
@@ -1,2 +1,38 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class MainUI {
+    private JPanel MainPanel;
+    private JButton homeButton;
+    private JButton bookingsButton;
+    private JButton roomsButton;
+    private JPanel Card;
+    private JPanel HomePanel;
+    private JPanel Bookins;
+    private JPanel Rooms;
+
+    public MainUI() {
+        homeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Card.removeAll();
+                Card.add(HomePanel);
+            }
+        });
+        bookingsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Card.removeAll();
+                Card.add(Bookins);
+            }
+        });
+        roomsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Card.removeAll();
+                Card.add(Rooms);
+            }
+        });
+    }
 }

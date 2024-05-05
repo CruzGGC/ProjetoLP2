cbd229d5158b3ef84664a3deebe82c35693e54a6
Guilherme <guilhermegcruz@ua.pt>
2024-05-05 03:09:51 +0100

    Tests (Important)

--- src/RoomBookingSystem.java
+++ src/RoomBookingSystem.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -6,15 +7,55 @@ import java.util.stream.Collectors;
 public class RoomBookingSystem {
 
     public List<Room> searchAvailableRooms(List<Room> rooms, List<Booking> bookings, int numberOfAdults, int numberOfChildren, LocalDate checkInDate, LocalDate checkOutDate, int canceledStatus) {
-        return rooms.stream()
-                .filter(room -> room.getAdultsCapacity() >= numberOfAdults && (room.getAdultsCapacity() + room.getChildrenCapacity()) >= (numberOfAdults + numberOfChildren))
-                .filter(room -> bookings.stream().noneMatch(booking -> booking.getRoomId() == room.getId() &&
-                        (checkInDate.isAfter(booking.getCheckInDate()) || checkInDate.isEqual(booking.getCheckInDate())) &&
-                        (checkInDate.isBefore(booking.getCheckOutDate()) || checkInDate.isEqual(booking.getCheckOutDate())) ||
-                        (checkOutDate.isAfter(booking.getCheckInDate()) || checkOutDate.isEqual(booking.getCheckInDate())) &&
-                                (checkOutDate.isBefore(booking.getCheckOutDate()) || checkOutDate.isEqual(booking.getCheckOutDate())) &&
-                                booking.getStatusId() != canceledStatus))
-                .sorted(Comparator.comparing(Room::getPrice))
-                .collect(Collectors.toList());
+        for (Booking booking : bookings) {
+            System.out.println("Guest First Name: " + booking.getGuestFirstName() + ", Guest Last Name: " + booking.getGuestLastName() + ", Room ID: " + booking.getRoomId() + ", Check-In Date: " + booking.getCheckInDate() + ", Check-Out Date: " + booking.getCheckOutDate() + ", Number of Adults: " + booking.getNumberOfAdults() + ", Number of Children: " + booking.getNumberOfChildren() + ", Status ID: " + booking.getStatusId());
+        }
+        //filter by room capacity
+        List<Room> availableRooms = rooms.stream()
+                .filter(room -> room.getAdultsCapacity() >= numberOfAdults && (room.getAdultsCapacity() + room.getChildrenCapacity()) >= (numberOfAdults + numberOfChildren)).collect(Collectors.toList());
+        List<Room> copy = new ArrayList<>(availableRooms);
+        for (Room room : copy) {
+            for (Booking booking : bookings) {
+                if (booking.getRoomId() == room.getId()) {
+                        /*
+                        ------|---------|------ <- booking
+                        ----|---|-------------- <- case 1
+                        ----------|---|-------- <- case 4 (completely eaten by the booking)
+                        --------------|----|--- <- case 3
+                        ----|-------------|---- <- case 4 (completely eats the booking)
+                        ------|---------|------ <- case 5 (exactly the same)
+                        */
+                        //case 1/2 - our checkout is in the middle of the booking / which fixes the case 4
+                        if (checkOutDate.compareTo(booking.getCheckInDate()) > 0 && checkOutDate.compareTo(booking.getCheckOutDate()) <= 0){
+                                // System.out.println("condition 1/2");
+                                availableRooms.remove(room);
+                        }
+                        //case 3 - our checkin is in the middle of the booking
+                        else if (checkInDate.compareTo(booking.getCheckInDate()) >= 0 && checkInDate.compareTo(booking.getCheckOutDate()) < 0){
+                                // System.out.println("condition 3");
+                                availableRooms.remove(room);
+                        }
+                        //case 4/5 - our booking completely eats the existing booking / exactly the same
+                        else if (checkInDate.compareTo(booking.getCheckInDate()) <= 0 && checkOutDate.compareTo(booking.getCheckOutDate()) >= 0){
+                                // System.out.println("condition 4/5");
+                                availableRooms.remove(room);
+                        }
+                }
+            }
+        }
+        return availableRooms.stream().sorted(Comparator.comparing(Room::getPrice)).collect(Collectors.toList());
+
+        // return availableRooms;
+        // return rooms.stream()
+        //     .filter(room -> room.getAdultsCapacity() >= numberOfAdults && (room.getAdultsCapacity() + room.getChildrenCapacity()) >= (numberOfAdults + numberOfChildren))
+        //     .filter(room -> bookings.stream().noneMatch(booking -> booking.getRoomId() == room.getId() &&
+        //                                                   (checkInDate.compareTo(booking.getCheckInDate()) >= 0 &&
+        //                                                    checkInDate.compareTo(booking.getCheckOutDate()) <= 0) ||
+        //                                                   (checkOutDate.compareTo(booking.getCheckInDate()) >= 1 &&
+        //                                                    checkOutDate.compareTo(booking.getCheckOutDate()) <= 0) &&
+        //                                                   booking.getStatusId() != canceledStatus))
+
+        //     .sorted(Comparator.comparing(Room::getPrice))
+        //     .collect(Collectors.toList());
     }
 }

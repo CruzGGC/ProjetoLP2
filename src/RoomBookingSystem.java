23870211ec5d2315b65ba27cf42e7d8ab44c6599
Guilherme <guilhermegcruz@ua.pt>
2024-05-05 03:25:03 +0100

    Teste Final (desisto)

--- src/RoomBookingSystem.java
+++ src/RoomBookingSystem.java
@@ -1,25 +1,7 @@
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
-
-/*public class RoomBookingSystem {
-
-    public List<Room> searchAvailableRooms(List<Room> rooms, List<Booking> bookings, int numberOfAdults, int numberOfChildren, LocalDate checkInDate, LocalDate checkOutDate, int canceledStatus) {
-        return rooms.stream()
-            .filter(room -> room.getAdultsCapacity() >= numberOfAdults && (room.getAdultsCapacity() + room.getChildrenCapacity()) >= (numberOfAdults + numberOfChildren))
-            .filter(room -> bookings.stream().noneMatch(booking -> booking.getRoomId() == room.getId() &&
-                                                          (checkInDate.compareTo(booking.getCheckInDate()) >= 0 &&
-                                                           checkInDate.compareTo(booking.getCheckOutDate()) <= 0) ||
-                                                          (checkOutDate.compareTo(booking.getCheckInDate()) >= 1 &&
-                                                           checkOutDate.compareTo(booking.getCheckOutDate()) <= 0) &&
-                                                          booking.getStatusId() != canceledStatus))
-
-            .sorted(Comparator.comparing(Room::getPrice))
-            .collect(Collectors.toList());
-    }
-}*/
 
 public class RoomBookingSystem {
     public List<Room> searchAvailableRooms(List<Room> rooms, List<Booking> bookings, int numberOfAdults, int numberOfChildren, LocalDate checkInDate, LocalDate checkOutDate, int canceledStatus) {
